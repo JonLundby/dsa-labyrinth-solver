c75c167ec166930fd448813f8c71829182edaecb
Jon <lundbyjon@hotmail.com>
2024-11-01 01:15:49 +0100

    added getNeighbours to fix endless recursion issue & css and view functions to mark route and deadends

--- controller.js
+++ controller.js
@@ -1,39 +1,101 @@
+"use strict";
+
 import * as view from "./view.js";
-import * as model from "./model.js";
 import Stack from "./stack.js";
 
-const labyrinth = model.labyrinthModel;
+window.addEventListener("load", init);
+
+let model;
 let startCell;
 let goalCell;
-let route = new Stack();
+let route = []; //new Stack();
 let strategy;
+let goalFound = false;
 
-init();
+async function init() {
+    // model sættes til at være javascript objekt parsed fra json
+    model = await getLabyrinthModel();
 
-function init() {
-    // bygger den visuelle labyrint og sætter start og goal cell
-    view.createLabyrinth(labyrinth);
-    startCell = labyrinth.maze[labyrinth.start.row][labyrinth.start.col];
-    goalCell = labyrinth.maze[labyrinth.goal.row][labyrinth.goal.col];
+    // bygger den visuelle labyrint/grid
+    view.createGrid(model);
+    view.showVisualLabyrinth(model);
 
-    console.log(labyrinth);
+    // sætter start og goal celler svarende til model/maze/labyrint
+    startCell = model.maze[model.start.row][model.start.col];
+    goalCell = model.goal;
 
-    document.querySelector("#dfs-solve-btn").addEventListener("click", () => {
-        visitCell(startCell);
+    // eventlisteners
+    document.querySelector("#solve-btn").addEventListener("click", () => {
+        visitCell(startCell); // start-cellen bliver sendt med som den første celle der besøges
     });
-    document.querySelector("#strategy-select").addEventListener("change", setStrategy);
+}
 
-    // pusher start cellen ind i route
-    // route.push(labyrinth.maze[labyrinth.start.row][labyrinth.start.col]);
-    // console.log("peek route: ", route.peek())
+// fetch model/json data
+async function getLabyrinthModel() {
+    const response = await fetch(`./labyrinth_02.json`);
+    const data = await response.json();
+
+    return data;
 }
 
 // Depth-First Search algoritmen
 function visitCell(cell) {
-    console.log("visit cell: ", cell);
+    
+    // markerer cellen som besøgt
+    cell.visited = true;
+    
+    // pusher cellen til route
+    route.push(cell);
+    // console.log("route after push: ", [...route]);
+
+    view.markVisitedCell(route, model)
+    
+    // tjekker om den celle der besøges er goal cellen
+    if (goalCell.row === cell.row && goalCell.col === cell.col) {
+        console.log("GOAL FOUND!!!");
+        goalFound = true;
+        view.markGoalRouteCells(route, model)
+        return;
+    }
+
+    // finder alle naboer der ikke er visited og som ikke er blokeret af en væg
+    let neighbours = getValidNeighbours(cell);
+
+    // rekursivt kald til visitCell så længe der er en tilgængelig nabo
+    while (neighbours.length > 0 && !goalFound) {
+        visitCell(neighbours[0])
+
+        neighbours = getValidNeighbours(cell)
+    }
+
+    // 
+    if (!goalFound) {
+        route.pop();
+    }
 }
 
-function setStrategy() {
-    strategy = document.querySelector("#strategy-select").value;
-    console.log("strategy: ", strategy);
+function getValidNeighbours(cell) {
+    let neighbours = [];
+
+    // Strategi for at tjekke vægge mod N, E, S, W
+    const directions = [
+        { name: "north", rowChange: -1, colChange: 0 },
+        { name: "west", rowChange: 0, colChange: -1 },
+        { name: "south", rowChange: 1, colChange: 0 },
+        { name: "east", rowChange: 0, colChange: 1 },
+    ];
+
+    // for hver mulig retning af cellen...
+    for (const dir of directions) {
+        if (!cell[dir.name]) {
+            const nextRow = cell.row + dir.rowChange;
+            const nextCol = cell.col + dir.colChange;
+
+            if (!model.maze[nextRow][nextCol].visited) {
+                neighbours.push(model.maze[nextRow][nextCol]);
+            }
+        }
+    }
+
+    return neighbours;
 }
--- view.js
+++ view.js
@@ -1,37 +1,68 @@
-export { createLabyrinth };
+export function createGrid(model) {
+    const labyrinthContainer = document.querySelector("#grid-container");
 
-function createLabyrinth(data) {
-    // console.log(data);
-    
-    // sætter html grid-container element til at være samme størrelse som labyrinten
-    document.documentElement.style.setProperty("--grid-rows", data.rows);
-    document.documentElement.style.setProperty("--grid-cols", data.cols);
-
-    const labContainer = document.querySelector("#grid-container");
-
-    for (let row = 0; row < data.rows; row++) {
-        for (let col = 0; col < data.cols; col++) {
+    for (let row = 0; row < model.rows; row++) {
+        for (let col = 0; col < model.cols; col++) {
+            // laver et nyt div element og adder css klassen cell
             const cell = document.createElement("div");
             cell.classList.add("cell");
-            if (data.maze[row][col].north) {
-                cell.classList.add("north");
-            }
-            if (data.maze[row][col].east) {
-                cell.classList.add("east");
-            } 
-            if (data.maze[row][col].south) {
-                cell.classList.add("south");
-            } 
-            if (data.maze[row][col].west) {
-                cell.classList.add("west");
-            }
-            if (data.goal.row === row && data.goal.col === col) {
-                cell.classList.add("goal")
-            }
-            if (data.start.row === row && data.start.col === col) {
-                cell.classList.add("start")
-            }
-            labContainer.insertAdjacentElement("beforeend", cell);
+
+            // indsætter cellen i containeren
+            labyrinthContainer.insertAdjacentElement("beforeend", cell);
+
+            document.documentElement.style.setProperty("--grid-rows", model.rows);
+            document.documentElement.style.setProperty("--grid-cols", model.cols);
         }
+    }
+}
+
+export function showVisualLabyrinth(model) {
+    const visualCells = document.querySelectorAll("#grid-container .cell");
+
+    for (let row = 0; row < model.rows; row++) {
+        for (let col = 0; col < model.cols; col++) {
+            const cell = model.maze[row][col];
+
+            const visualCellIndex = row * model.cols + col;
+            const visualCell = visualCells[visualCellIndex];
+
+            // tilføjer klasser til cellen baseret på hvilke vægge der er i cellen
+            if (cell.north) {
+                visualCell.classList.add("north");
+            }
+            if (cell.east) {
+                visualCell.classList.add("east");
+            }
+            if (cell.south) {
+                visualCell.classList.add("south");
+            }
+            if (cell.west) {
+                visualCell.classList.add("west");
+            }
+            if (model.goal.row === row && model.goal.col === col) {
+                visualCell.classList.add("goal");
+            }
+            if (model.start.row === row && model.start.col === col) {
+                visualCell.classList.add("start");
+            }
+        }
+    }
+}
+
+export function markVisitedCell(route, model) {
+    const visualCells = document.querySelectorAll("#grid-container .cell");
+    for (const cell of route) {
+        const visualCellIndex = cell.row * model.cols + cell.col;
+        const visualCell = visualCells[visualCellIndex];
+        visualCell.classList.add("visited")
+    }
+}
+
+export function markGoalRouteCells(route, model) {
+    const visualCells = document.querySelectorAll("#grid-container .cell");
+    for (const cell of route) {
+        const visualCellIndex = cell.row * model.cols + cell.col;
+        const visualCell = visualCells[visualCellIndex];
+        visualCell.classList.add("route")
     }
 }
